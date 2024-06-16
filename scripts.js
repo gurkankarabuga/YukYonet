722899ea4eab2de6b086b9d63434fab16a38d7b1
gurkankarabuga <a.gurkankarabuga@gmail.com>
2024-06-17 02:26:54 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const truck = document.getElementById('truck');
-    const floatingShip = document.getElementById('floating-ship');
-    const airplane = document.getElementById('airplane');
-    const train = document.getElementById('train');
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
 
-    function resetAnimation(element, animationName) {
-        element.style.animation = 'none';
-        element.offsetHeight; // Trigger reflow
-        element.style.animation = null;
-    }
-
-    function syncAnimations() {
-        resetAnimation(truck, 'drive');
-        resetAnimation(floatingShip, 'sail');
-        resetAnimation(airplane, 'fly');
-        resetAnimation(train, 'driveTrain');
-    }
-
-    syncAnimations();
-});
+	
