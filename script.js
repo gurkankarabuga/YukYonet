9c4f1e4aa3e0b23c5cb0eee2c8a6fd5fb3663200
gurkankarabuga <a.gurkankarabuga@gmail.com>
2024-06-17 03:31:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function onClickMenu(){
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
-	
-	document.getElementById("menu-bg").classList.toggle("change-bg");
+// Menü açma/kapama işlemi
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
-	
+// Scroll to top
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.querySelector(".to-top").style.display = "block";
+    } else {
+        document.querySelector(".to-top").style.display = "none";
+    }
+}
+
+document.querySelector(".to-top").addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
